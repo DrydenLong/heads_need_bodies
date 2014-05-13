052bd06130c2f8d70b09941ff4b41740de598826
DrydenLong <DrydenLong@users.noreply.github.com>
2014-05-13 10:16:30 -0700

    Update bodybot.py
    cleaned up body code. now that we're using code formatting, we no longer need to insert spaces as unicode.

--- bodybot.py
+++ bodybot.py
@@ -7,20 +7,8 @@ r.login('username', 'password')
 already_done = []
 all_comments = r.get_comments('all')
 
-#Here we define the "spaces" to shorten the code of the bodies
-
-sp = u'\xa0'
-sp2 = u'\xa0'u'\xa0'
-sp3 = u'\xa0'u'\xa0'u'\xa0'
-sp4 = u'\xa0'u'\xa0'u'\xa0'u'\xa0'
-sp5 = u'\xa0'u'\xa0'u'\xa0'u'\xa0'u'\xa0'
-hair = u'\u200A'
-thin = u'\u2009'
 intro = u'\u0020'u'\u0020'u'\u0020'u'\u0020'
 new = u'\u0020'u'\u0020'+'\n'
-
-
-#All the heads/faces we are looking for
 
 head1 = u'ಠ_ಠ'.encode('utf-8')
 head2 = u'° ͜ʖ ͡°'.encode('utf-8')
@@ -41,14 +29,13 @@ head16 = u'ಠ╭╮ಠ'.encode('utf-8')
 head17 = u'⌐■_■'.encode('utf-8')
 head18 = u'•_•'.encode('utf-8')
 head19 = u'◉ω◉'.encode('utf-8')
+head20 = u'ﾟ∀ﾟ'.encode('utf-8')
 
-#Some heads just don't work with some body types, so we create 2 lists
-
-heads = [head1, head2, head3, head4, head5, head6, head7, head8, head9, head10, head11, head12, head13, head14, head15, head16, head17, head18, head19]
-small_heads = [head1, head4, head5, head6, head7, head12, head13, head15, head16, head17, head18, head19]
+heads = [head1, head2, head3, head4, head5, head6, head7, head8, head9, head10, head11, head12, head13, head14, head15, head16, head17, head18, head19, head20]
+small_heads = [head1, head4, head5, head6, head7, head12, head13, head15, head16, head17, head18, head19, head20]
 
 while True:
-    all_comments = r.get_comments('Funny+Pics+AdviceAnimals')   
+    all_comments = r.get_comments('Funny+Pics+AdviceAnimals+4ChanMeta')   
     for comment in all_comments:
         if not hasattr(comment, 'body'):
             continue
@@ -58,125 +45,146 @@ while True:
                 for head in heads:
                     if head in comment.body.encode('utf-8'):
                         if head in small_heads:
-                            library = 'all_bodies'
+                            library = 'small_heads'
                         else:
                             library = 'large_heads'
                         head = head.decode('utf-8')
                         body1 = (intro+head+new+
-                                 intro+sp+'| '+new+
-                                 intro+'/|\ '+new+
-                                 intro+sp+'| '+new+
-                                 intro+sp+'LL').encode('utf-8')
-                        body2 = (intro+'|'+sp3+'o\\ '+new+
-                                 intro+'|'+sp+'W'+sp3+'\\'+head+new+
-                                 intro+'|'+sp5+sp2+'|\\ '+new+
-                                 intro+'|'+sp5+sp+'/-\\ '+new+
-                                 intro+'|'+sp4+'/'+sp5+'\\ '+new+
+                                 intro+' |'+new+
+                                 intro+'/|\\'+new+
+                                 intro+' |'+new+
+                                 intro+' LL').encode('utf-8')
+
+                        body2 = (intro+'|  o\\'+new+
+                                 intro+'|W   \\'+head+new+
+                                 intro+'|      |\\'+new+
+                                 intro+'|     /-\\'+new+
+                                 intro+'|   /     \\'+new+
                                  intro+'|'+new+
                                  intro+'|').encode('utf-8')
-                        body3 = (intro+sp+head+new+
+
+                        body3 = (intro+' '+head+new+
                                  intro+'/-|--/`'+new+
-                                 intro+'\\'+sp+'|'+new+
-                                 intro+sp2+'/--i'+new+
-                                 intro+sp+'/'+sp3+'L'+new+
-                                 intro+sp+'L').encode('utf-8')
-                        body4 = (intro+sp3+'__'+head+new+
-                                 intro+sp+'_'+sp+'\<_'+new+
+                                 intro+'\ |'+new+
+                                 intro+'  /--i'+new+
+                                 intro+' /   L'+new+
+                                 intro+' L').encode('utf-8')
+
+                        body4 = (intro+'   __'+head+new+
+                                 intro+' _ \<_'+new+
                                  intro+'(_)/(_)').encode('utf-8')
+
                         body5 = (intro+head+new+
                                  intro+'/|\\'+new+
                                  intro+'(o-"='+new+
-                                 intro+'/'+sp+'\\').encode('utf-8')
-                        body6 = (intro+sp2+head+'_/'+new+
-                                 intro+sp+'_/|'+new+
-                                 intro+sp+'__)\\'+new+
-                                 intro+'`'+sp4+'\\'+sp3+'o').encode('utf-8')
-                        body7 = (intro+sp5+sp4+head+sp+','+sp5+'o__,'+new+
-                                 intro+sp5+sp5+'[</'+sp5+sp+'[\/'+new+
-                                 intro+sp5+'(`\---/--------/----\')'+new+
-                                 intro+'\~\~\~\~\~@\~\~\~\~@\~\~\~\~\~\~\~\~').encode('utf-8')
-                        body8 = (intro+sp5+sp3+thin+'__'+new+
-                                 intro+sp5+sp2+thin+'/'+sp2+'\\'+new+
-                                 intro+sp5+sp2+thin+'\\'+sp2+'/'+new+
-                                 intro+sp5+sp2+thin+'/\/'+new+
-                                 intro+sp5+sp+thin+'/'+sp2+'%'+new+
-                                 intro+sp5+thin+'/'+sp5+'%'+new+
-                                 intro+head+sp+'/'+new+
-                                 intro+sp+'|_/'+new+
+                                 intro+'/ \\').encode('utf-8')
+
+                        body6 = (intro+'  '+head+'_/'+new+
+                                 intro+' _/|'+new+
+                                 intro+' __)\\'+new+
+                                 intro+'`    \  o').encode('utf-8')
+
+                        body7 = (intro+'       '+head+' ,    o__'+new+
+                                 intro+'        [</     [\/'+new+
+                                 intro+'    (`---/-------/----\')'+new+
+                                 intro+'\~\~\~\~@\~\~\~\@~\~\~\~\~\~\~\~').encode('utf-8')
+
+                        body8 = (intro+'        __'+new+
+                                 intro+'       /  \\'+new+
+                                 intro+'       \  /'+new+
+                                 intro+'       /\/'+new+
+                                 intro+'      /  %'+new+
+                                 intro+'     /    %'+new+
+                                 intro+head+' /'+new+
+                                 intro+' |_/'+new+
                                  intro+'/|'+new+
-                                 intro+'/'+sp+'\\').encode('utf-8')
-                        body9 = (intro+head+sp4+'________________'+new+
-                                intro+'/\_'+sp2+'_|'+sp5+sp5+sp5+sp+'|'+new+
-                                intro+'\__`[__________________|'+new+
-                                intro+']'+sp+'[\,/]['+sp5+sp5+sp3+'][').encode('utf-8')
-                        body10 = (intro+sp5+sp+'|'+new+
-                                  intro+sp4+hair+'/'+sp3+'\\'+new+
-                                  intro+sp3+thin+'/'+sp5+'\\'+new+
-                                  intro+sp2+'(__'+head+'__)'+new+
-                                  intro+sp4+'|||||'+new+
-                                  intro+sp4+'|||||'+new+
-                                  intro+sp+'\_///|'+sp+'\\__/'+new+
-                                  intro+sp2+'_//'+sp+'|'+sp2+'\_'+new+
-                                  intro+sp2+'/'+sp2+'/').encode('utf-8')
-                        body11 = (intro+sp5+sp5+'.----.'+new+
-                                  intro+sp+'____'+sp4+'__\\\\\\\\\\\\__'+new+
-                                  intro+sp+'\___\'--"'+sp5+sp4+'.-.\\'+new+
-                                  intro+sp+'/___<'+sp5+sp5+'\''+sp+head+'\''+new+
-                                  intro+'/____,--.'+sp5+sp2+'y'+sp4+'/'+new+
-                                  intro+sp5+sp3+'"".____'+sp2+'_-"'+new+
-                                  intro+sp5+sp3+'//'+sp4+'/'+sp+'/'+new+
-                                  intro+sp5+sp5+sp4+']/').encode('utf-8')
-                        body12 = (intro+sp+'\\'+sp3+head+sp3+'/'+new+
-                                  intro+sp2+'\\'+sp+'__|__'+sp+'/'+new+
-                                  intro+sp3+'[____]]'+new+
-                                  intro+sp4+'__|___'+new+
-                                  intro+sp3+'/'+sp4+'/\\'+new+
-                                  intro+sp2+'/'+sp4+'/'+sp2+'\\'+new+
-                                  intro+sp+'(____(____)').encode('utf-8')
-                        body13 = (intro+sp+'O'+new+
-                                  intro+'/\\'+sp+'__'+head+new+
-                                  intro+'\|`>'+sp2+'\\').encode('utf-8')
+                                 intro+'/ \\').encode('utf-8')        
+
+                        body9 = (intro+head+'   ________________'+new+
+                                 intro+'/\_  _|              |'+new+
+                                 intro+'\__`[________________|'+new+
+                                 intro+'] [\,/][           ][').encode('utf-8')
+
+                        body10 = (intro+'     |'+new+
+                                  intro+'   /   \\'+new+  
+                                  intro+'  /     \\'+new+      
+                                  intro+' (__'+head+'__)'+new+
+                                  intro+'   |||||'+new+
+                                  intro+'   |||||'+new+
+                                  intro+'\_///| \\__/'+new+
+                                  intro+' _// |  \_'+new+
+                                  intro+'  / /').encode('utf-8')
+                                  
+                        body11 = (intro+'          .----.'+new+
+                                  intro+' ____    __\\\\\\__'+new+
+                                  intro+' \___\'--"         .-.'+new+
+                                  intro+' /___<          \' '+head+'\''+new+
+                                  intro+'/____,--.       y   B'+new+
+                                  intro+'        "".____  _-"'+new+
+                                  intro+'        //    / /'+new+
+                                  intro+'              ]/').encode('utf-8')
+
+                        body12 = (intro+'\   '+head+'   /'+new+
+                                  intro+' \ __|__ /'+new+
+                                  intro+'  [____]]'+new+ 
+                                  intro+'   __|__'+new+ 
+                                  intro+'  /    /\\'+new+  
+                                  intro+' /    /  \\'+new+
+                                  intro+'(____(____)').encode('utf-8')
+
+                        body13 = (intro+' O'+new+    
+                                  intro+'/\__'+head+new+
+                                  intro+'> > \\').encode('utf-8')
+
                         body14 = (intro+head+new+
                                   intro+'<|>'+new+
                                   intro+'/'+u'ω'+'\\').encode('utf-8')
-                        body15 = (intro+sp+'(\\'+sp+'/)'+new+
-                                  intro+sp+'('+head+')'+new+
-                                  intro+thin+thin+u'ᑕ'+'('+sp2+')'+u'ᑐ'+new+
-                                  intro+thin+'(")'+sp+'(")').encode('utf-8')
-                        body16 = (intro+sp2+'_'+new+
+
+                        body15 = (intro+' (\ /)'+new+
+                                  intro+' ('+head+')'+new+
+                                  intro+' '+u'ᑕ'+'( )'+u'ᑐ'+new+
+                                  intro+'(") (")').encode('utf-8')
+
+                        body16 = (intro+'  _'+new+
                                   intro+'_|_|_'+new+
                                   intro+'{'+head+'}'+new+
                                   intro+'/|O|\\'+new+
-                                  intro+'\\'+hair+'[|]'+hair+'/'+new+
-                                  intro+sp+'/'+sp+'\\'+new+
-                                  intro+sp+'|'+sp+'|'+new+
-                                  intro+'_|'+sp+'|_').encode('utf-8')
-                                  
-                        body17 = ("""\
-                        
-                                           ._ o o
-                                           \_`-)|_
-                                        ,""       \ 
-                                      ,"  ## |   ಠ ಠ. 
-                                    ," ##   ,-\__    `.
-                                  ,"       /     `--._;)
-                                ,"     ## /
-                              ,"   ##    /
-                        
-                        
-                        """).encode('utf-8')
+                                  intro+'\[|]/'+new+
+                                  intro+' / \\'+new+
+                                  intro+' | |'+new+
+                                  intro+'_| |_').encode('utf-8')
 
-                        if library == 'all_bodies':
-                            bodies = [body1, body2, body3, body4, body5, body6, body7, body8, body9, body10, body11, body12, body13, body14, body15, body16, body17]
+                        body17 = (intro+'___    _________________________________________'+new+
+                                  intro+'|  |==|  |     |                                 \\'+new+
+                                  intro+'|  ||||  |+---+|+-----+-----+-----+-----+   +-----\\'+new+
+                                  intro+'|  ||||  ||   |||     |     |     |     |   |  '+head+' \\'+new+
+                                  intro+'|  ||||  ||   |||_____|_____|_____|_____|   |_/|o|\_\\'+new+
+                                  intro+'|  ||||  ||___||===== heads_need_bodies Express =====\\'+new+
+                                  intro+'|__|==|__|_____|_____________________________________|[}'+new+
+                                  intro+'         ( O O )                           ( O O )'+new+
+                                  intro+'==============================================================').encode('utf-8')
+
+                        body18 = (intro+'             ._ o o'+new+
+                                  intro+'             \_`-)|_'+new+
+                                  intro+'          ,""       \\'+new+ 
+                                  intro+'        ,"  ## |   '+head+'.'+new+ 
+                                  intro+'      ," ##   ,-\__    `.'+new+
+                                  intro+'    ,"       /     `--._;)'+new+
+                                  intro+'  ,"     ## /'+new+
+                                  intro+',"   ##    /').encode('utf-8')
+                
+                        if library == 'small_heads':
+                            bodies = [body1, body2, body3, body4, body5, body6, body7, body8, body9, body10, body11, body12, body13, body14, body15, body16, body17, body18]
                         else:
-                            bodies = [body1, body2, body3, body4, body5, body6, body7, body8, body9, body11, body13, body14, body15, body17]
+                            bodies = [body1, body2, body3, body4, body5, body6, body7, body8, body9, body11, body13, body14, body15]
                         
-                        #Several Reddit users have sent in bodies to use, here we give them credit
                         hotshotjosh = [body10, body11]
                         scored420yearsago = [body12]
                         chbay = [body14]
                         happyfacekillah = [body15]
                         tyx = [body16]
+                        ChromeLynx = [body17]
+                        herminator = [body18]
                         bod = random.choice(bodies)
                         if bod in hotshotjosh:
                             credit = ('\n\n'+'Credit to /u/hotshotjosh for sending me this body.')
@@ -186,9 +194,165 @@ while True:
                             credit = ('\n\n'+'Credit to /u/chbay for sending me this body.')
                         elif bod in happyfacekillah:
                             credit = ('\n\n'+'Credit to /u/happyfacekillah for sending me this body.')
-                        if bod in tyx:
+                        elif bod in tyx:
                             credit = ('\n\n'+'Credit to /u/Tyx for sending me this body.')
+                        elif bod in ChromeLynx:
+                            credit = ('\n\n'+'Credit to /u/ChromeLynx for sending me this body.')
+                        elif bod in herminator:
+                            credit = ('\n\n'+'Credit to /u/herminator for this body.')
                         else:
                             credit = ('')
                         comment.reply(bod+credit+'\n\n')
                         print comment.id
+                    
+
+
+
+
+
+
+#body1
+#ಠ_ಠ
+# |
+#/|\
+# |
+# LL
+
+#body2
+#|  o\
+#|W   \ಠ_ಠ
+#|      |\
+#|     /-\
+#|   /     \
+#|
+#|
+
+#body3
+# ಠ_ಠ
+#/-|--/`
+#\ |
+#  /--i
+# /   L
+# L
+
+#body4
+#   __ಠ_ಠ
+# _ \<_
+#(_)/(_)
+
+#body5
+#ಠ_ಠ
+#/|\
+#(o-"=
+#/ \
+
+#body6
+#  ಠ_ಠ_/
+# _/|
+# __)\
+#`    \  o
+
+#body7
+#       ಠ_ಠ,     o__
+#        [</     [\/
+#    (`---/-------/----')
+#\~\~\~@\~\~\~\~@\~\~\~\~\~\~\~\~
+
+#body8
+#        __
+#       /  \
+#       \  /
+#       /\/
+#      /  %
+#     /    %
+#ಠ_ಠ / 
+# |_/
+#/|
+#/ \        
+
+#body9
+#ಠ_ಠ   ________________
+#/\_  _|              |
+#\__`[________________|
+#] [\,/][           ][
+
+#body10
+#     |
+#   /   \       
+#  /     \      
+# (__ಠ_ಠ__)
+#   |||||
+#   |||||
+#\_///| \\__/
+# _// |  \_
+#  / /
+          
+#body11
+#          .----.
+# ____    __\\\\\\__
+# \___'--"         .-.
+# /___<          ' ಠ_ಠ'
+#/____,--.       y   B
+#        "".____  _-"
+#        //    / /
+#              ]/
+
+#body12
+#\   ಠᴗಠ   /
+# \ __|__ /
+#  [____]]  
+#   __|__  
+#  /    /\  
+# /    /  \  
+#(____(____)
+
+#body13
+# O    
+#/\__O
+#> > \
+
+#body14
+#ಠ_ಠ
+#<|>
+#/ω\
+
+#body15
+# (\ /)
+# (ಠ_ಠ)
+# ᑕ( )ᑐ
+#(") (")
+
+#body16
+#  _
+#_|_|_
+#{ಠ_ಠ}
+#/|O|\
+#\[|]/
+# / \
+# | |
+#_| |_
+
+#body17
+#             
+#___    _________________________________________
+#|  |==|  |     |                                 \
+#|  ||||  |+---+|+-----+-----+-----+-----+   +-----\
+#|  ||||  ||   |||     |     |     |     |   |  ಠ_ಠ \
+#|  ||||  ||   |||_____|_____|_____|_____|   |_/|o|\_\
+#|  ||||  ||___||===== heads_need_bodies Express =====\
+#|__|==|__|_____|_____________________________________|[}
+#         ( O O )                           ( O O )
+#==============================================================
+#*  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *
+                        
+#body18
+#              ._ o o
+#              \_`-)|_
+#           ,""       \ 
+#        ,"  ## |   ಠ ಠ. 
+#      ," ##   ,-\__    `.
+#    ,"       /     `--._;)
+#  ,"     ## /
+#,"   ##    /
+
+
